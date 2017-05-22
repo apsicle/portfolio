e12f74745d2c1c2bf609671a98983c6edac4efc5
apsicle <rcyan@email.wm.edu>
2017-05-22 14:53:17 -0400

    fixed resolution problem actually. Fixed aspect ratio scaling for videos. Added some design features

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 let n = 5; //number of images, i.e. sides of the carousel
 let panel_width = window.innerWidth * .4167; //width of a panel, i.e. length of a side on the polygon
+let ratio_mismatch = false;
+if(Math.abs((window.innerWidth / window.innerHeight) - (1920 / 1012)) > .20) {
+	// if the aspect ratio of the viewport and the video is off, since you can't change the aspect ratio of
+	// the video on the fly, the video sometimes will not completely cover the image it's laid on top of.
+	// In this case, we'll hide the image when the user hovers over the image.
+	ratio_mismatch = true;
+	$('.carousel_image').css("transition", "opacity 0.5s ease-out");
+}
 
 let active_page = document.getElementById("carousel_page")
 let carousel = document.getElementById("carousel");
@@ -51,18 +59,33 @@ function previous() {
 }
 
 function hoverVideo(e) { 
+	// show video
 	let vid = $('.overlay', this).get(0);
 	vid.style.opacity = 1;
 	vid.style.transform = "scale(1.15, 1.15)";
 	vid.play();
+
+	// hide the image
+	if(ratio_mismatch) {
+		let img = $('img', this).get(0);
+		img.style.opacity = 0;
+	}
+	
 }
 
 function hideVideo(e) {
+	// hide video
 	let vid = $('.overlay', this).get(0);
 	vid.style.opacity = 0;
 	vid.style.transform = "scale(1, 1)";
     vid.pause();
     vid.currentTime = 0;
+
+    // show image
+	if(ratio_mismatch) {
+		let img = $('img', this).get(0);
+		img.style.opacity = 1;
+	}
 }
 
 function refresh_content(div_obj, content) {
@@ -81,42 +104,46 @@ function display_info(num) {
 
 	switch(num) {
 		case 0:
-			refresh_content(title, "<a class='my_link' href='https://github.com/suhuiii/Colorburst'>Rain Drops</a>");
-			refresh_content(description, "<p> Inspired by summer storms that add a splash of color into long, \
+			refresh_content(title, "<br><a class='my_link' href='https://github.com/suhuiii/Colorburst'>Rain Drops</a>");
+			refresh_content(description, "<br>Inspired by summer storms that add a splash of color into long, \
 			hot, summer days. Created in p5. The default image is from <a class='my_link' \
 			href='http://www.zerochan.net/1431763'>here</a> Note: suhuiii and I turned this into a chrome new tab \
 			extension with random images from Unsplash.");
 			break;
 		case 1:
-			refresh_content(title, "<a class='my_link' href='https://github.com/apsicle/octosoccer'>Octosoccer</a>");
-			refresh_content(description, "<p>Octosoccer was an experiment in creating an online multiplayer game. \
+			refresh_content(title, "<br><a class='my_link' href='https://github.com/apsicle/octosoccer'>Octosoccer</a>");
+			refresh_content(description, "<br>Octosoccer was an experiment in creating an online multiplayer game. \
 			Sprint, score, juke, trash talk. Download <a class='my_link' href='files/octosoccer_client.exe'>the game</a>.\
-			Made with Lua, sock.lua (networking), bitser (serialization), LÖVE, Piskel, and GIMP</p>");
+			Made with Lua, sock.lua (networking), bitser (serialization), LÖVE, Piskel, and GIMP");
 
 			break;
 		case 2:
-			refresh_content(title, "<a class='my_link' href='https://github.com/apsicle/8bitifier'>8-Bitifier</a>");
-			refresh_content(description, "<p>Turns your everyday images into 8-bit art! Written in Python and Flask,\
-			using scipy, hosted on <a class='my_link' href='http://apsicle.pythonanywhere.com/main'>pythonanywhere</a>.</p>");
+			refresh_content(title, "<br><a class='my_link' href='https://github.com/apsicle/8bitifier'>8-Bitifier</a>");
+			refresh_content(description, "<br>Turns your everyday images into 8-bit art! Written in Python and Flask,\
+			using scipy, hosted on <a class='my_link' href='http://apsicle.pythonanywhere.com/main'>pythonanywhere</a>.");
 			break;
 		case 3:
-			refresh_content(title, "<a class='my_link' href='https://github.com/apsicle/crimson'>Crimson</a>");
-			refresh_content(description, "<p>Elemental spell-based dungeoncrawler / roguelike (on hold). Combine \
-			elements to cast unique spells and target elemental weaknesses. Made with Lua, LÖVE, Piskel, and GIMP.</p>");
+			refresh_content(title, "<br><a class='my_link' href='https://github.com/apsicle/crimson'>Crimson</a>");
+			refresh_content(description, "<br>Elemental spell-based dungeoncrawler / roguelike (on hold). Combine \
+			elements to cast unique spells and target elemental weaknesses. Made with Lua, LÖVE, Piskel, and GIMP.");
 			break;
 		case 4:
-			refresh_content(title, "<a class='my_link' href='https://github.com/apsicle/happy_holidays'>Happy Holidays</a>");
-			refresh_content(description,"<p>Inspired by a friend of mine who is so busy but always has time to care \
+			refresh_content(title, "<br><a class='my_link' href='https://github.com/apsicle/happy_holidays'>Happy Holidays</a>");
+			refresh_content(description,"<br>Inspired by a friend of mine who is so busy but always has time to care \
 			for the people around her. Since I'm no good at crafts, I decided to make her a Christmas card while \
-			exploring CSS transitions, HTML canvas, and object-oriented programming in Javascript.</p>");
+			exploring CSS transitions, HTML canvas, and object-oriented programming in Javascript.");
 			break;
 		}
-
-
-
 }
 
+/* Note: this only waits for DOM objects to load. 
 $(document).ready(function() {
+	$(active_page).get(0).style.background = "black";
+	calc_angles();
+}); */
+
+/* We need to wait for images, fonts, videos to load. That's this function: */
+$(window).load(function() {
 	$(active_page).get(0).style.background = "black";
 	calc_angles();
 });
